c67d328c2970c4fba1505e1c2a4c77fc013eebc8
arora <aroramadhav65@gmail.com>
2022-03-11 14:35:02 +0530

    update

--- lib/Screens/Blocked_Contacts_Screen/Blocked_Contacts_Screen.dart
+++ lib/Screens/Blocked_Contacts_Screen/Blocked_Contacts_Screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:whatsapp/Constants/Constants.dart';
@@ -28,51 +28,54 @@ class _BlockedContactsScreenState extends State<BlockedContactsScreen> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Contacts',
-            style: TextStyle(
-              fontSize: 17,
-              color: grey,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ListTile(
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (BuildContext context) => ChatScreen(
-              //       avatar: dummyData[i].avatarUrl,
-              //       name: dummyData[i].name,
-              //       phoneno: dummyData[i].phoneno,
-              //       about: dummyData[i].about,
-              //     ),
-              //   ),
-              // );
-            },
-            leading: CircleAvatar(
-              foregroundColor: Theme.of(context).primaryColor,
-              backgroundColor: grey,
-              child: Icon(
-                Icons.person,
-                color: white,
-                size: 30,
-              ),
-            ),
-            title: Text(
-              '+91' '7011698878',
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Contacts',
               style: TextStyle(
+                fontSize: 17,
+                color: grey,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (BuildContext context) => ChatScreen(
+                //       avatar: dummyData[i].avatarUrl,
+                //       name: dummyData[i].name,
+                //       phoneno: dummyData[i].phoneno,
+                //       about: dummyData[i].about,
+                //     ),
+                //   ),
+                // );
+              },
+              leading: CircleAvatar(
+                foregroundColor: Theme.of(context).primaryColor,
+                backgroundColor: grey,
+                child: Icon(
+                  Icons.person,
+                  color: white,
+                  size: 30,
+                ),
+              ),
+              title: Text(
+                '+91' ' ' '7011698878',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
