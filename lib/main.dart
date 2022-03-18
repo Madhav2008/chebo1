f44e58bb047e4220ef55c0a88232efb66a88ce25
arora <aroramadhav65@gmail.com>
2022-03-18 17:39:07 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
                 name: 'Madhav Arora',
                 avatar:
                     'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
-              //   phoneno: '9999348666',
+                phoneno: '9999348666',
               //   countryCode: '+91',
               //   about: 'Hey! there I am using WhatsApp India.',
               // ),
