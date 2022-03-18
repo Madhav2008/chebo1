f8b0fc6f74b00f43e1832f9f8e1a006afbb7a32c
arora <aroramadhav65@gmail.com>
2022-03-18 17:40:49 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class MyApp extends StatelessWidget {
               //   countryCode: '+91',
               //   about: '👋🏻 Hey! there I am using WhatsApp India.',
               // ),
-              // SplashScreen(),
+              SplashScreen(),
             ),
           );
         },
