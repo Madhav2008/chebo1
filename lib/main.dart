c80242c3e0575f0b56270d425e8959b1f42f0fb8
arora <aroramadhav65@gmail.com>
2022-03-18 17:38:59 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
                 cameras: cameras,
                 name: 'Madhav Arora',
                 avatar:
-              //       'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
+                    'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
               //   phoneno: '9999348666',
               //   countryCode: '+91',
               //   about: 'Hey! there I am using WhatsApp India.',
