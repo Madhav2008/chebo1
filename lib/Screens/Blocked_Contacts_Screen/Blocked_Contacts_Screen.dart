faf46a42f51bd1f142bf8ceebea96fb9c7c0b364
arora <aroramadhav65@gmail.com>
2022-03-19 15:06:30 +0530

    Update

--- lib/Screens/Blocked_Contacts_Screen/Blocked_Contacts_Screen.dart
+++ lib/Screens/Blocked_Contacts_Screen/Blocked_Contacts_Screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:whatsapp/Constants/Constants.dart';
+import 'package:whatsapp/Screens/Chat_Screen/Chat_Screen.dart';
 
 class BlockedContactsScreen extends StatefulWidget {
   const BlockedContactsScreen({Key? key}) : super(key: key);
