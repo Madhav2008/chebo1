d0a514cf09b330c6eb9a1226c6e466257e051b93
arora <aroramadhav65@gmail.com>
2022-03-19 15:50:10 +0530

    Update

--- lib/Screens/Media_Links_And_Docs/Media_Links_And_Docs.dart
+++ lib/Screens/Media_Links_And_Docs/Media_Links_And_Docs.dart
@@ -51,7 +51,7 @@ class _MediaLinksAndDocsState extends State<MediaLinksAndDocs>
           },
           body: TabBarView(
             children: <Widget>[
-              Center(child: Text('Media')),
+              Center(child: Text('Media'),),
               Text('Docs'),
               Text('Links'),
             ],
