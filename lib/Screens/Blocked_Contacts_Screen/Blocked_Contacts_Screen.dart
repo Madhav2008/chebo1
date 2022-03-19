2488b20cb6cdf46ee04f42035214233ac9b4206f
arora <aroramadhav65@gmail.com>
2022-03-19 15:07:50 +0530

    Update

--- lib/Screens/Blocked_Contacts_Screen/Blocked_Contacts_Screen.dart
+++ lib/Screens/Blocked_Contacts_Screen/Blocked_Contacts_Screen.dart
@@ -57,7 +57,7 @@ class _BlockedContactsScreenState extends State<BlockedContactsScreen> {
                         // avatar: dummyData[i].avatarUrl,
                         // name: dummyData[i].name,
                         // phoneno: dummyData[i].phoneno,
-                        about: dummyData[i].about,
+                        // about: dummyData[i].about,
                       );
                     },
                   ),
