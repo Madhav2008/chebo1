b621bb26b78012d505d64a48a3da8e00d734d47f
arora <aroramadhav65@gmail.com>
2022-03-20 19:14:05 +0530

    Update

--- lib/Auth/Wrapper.dart
+++ lib/Auth/Wrapper.dart
@@ -23,7 +23,7 @@ class Wrapper extends StatelessWidget {
       builder: (_,AsyncSnapshot<User?>snapshot){
         if(snapshot.connectionState==ConnectionState.active){
           final User?user=snapshot.data;
-          return user == null ? LandingPage() : NavigationScreen(cameras: [],);
+          return user == null ? LandingPage() : NavigationScreen(cameras: [], name: 'Madhav Arora',);
         } else {
           return Scaffold(
               body: Center(
