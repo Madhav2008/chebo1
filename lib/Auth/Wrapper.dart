636e4f79b046945866f7270a668c3bb71d01d73c
arora <aroramadhav65@gmail.com>
2022-03-20 19:17:04 +0530

    Update

--- lib/Auth/Wrapper.dart
+++ lib/Auth/Wrapper.dart
@@ -31,7 +31,8 @@ class Wrapper extends StatelessWidget {
                   about: '👋🏻 Hey! there I am using WhatsApp India.',
                   avatar:
                       'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
-                  countryCode: '+91', phoneno: '9999348444',
+                  countryCode: '+91',
+                  phoneno: '9999348444',
                 );
         } else {
           return Scaffold(
