09ca71ac896083626f6261f2146eda5b2241d1dd
arora <aroramadhav65@gmail.com>
2022-03-22 14:43:17 +0530

    Update

--- lib/Widgets/OwnMessageCard.dart
+++ lib/Widgets/OwnMessageCard.dart
@@ -42,6 +42,7 @@ class OwnMessageCard extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(8),
+              topRight: Radius.circular(8),
             ),
           ),
           color: color,
