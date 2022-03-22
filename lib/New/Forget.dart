b1f2716a7d42584c7509469cf29d9e070f5e4dc0
arora <aroramadhav65@gmail.com>
2022-03-22 23:05:31 +0530

    Update

--- lib/New/Forget.dart
+++ lib/New/Forget.dart
@@ -118,7 +118,6 @@ class _LoginScreenState extends State<ForgetScreen> {
                     child: Container(
                       width: MediaQuery.of(context).size.width * 0.5,
                       height: 50,
-                      // ignore: deprecated_member_use
                       child: RaisedButton(
                         color: one,
                         onPressed: () {},
