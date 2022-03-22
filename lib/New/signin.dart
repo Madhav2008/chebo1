f93611e7bf1f931b2abf1a34037359640a12ace7
arora <aroramadhav65@gmail.com>
2022-03-22 19:36:04 +0530

    Update

--- lib/New/signin.dart
+++ lib/New/signin.dart
@@ -50,7 +50,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 children: [
                   Padding(
                     padding: EdgeInsets.only(top: 10.0),
-                    child: Image.network(
+                    child: Image.asset(
                       "https://github.com/Madhav2008/App-Assets/blob/main/RecipoLogo.png?raw=true",
                       width: 100,
                       height: 100,
