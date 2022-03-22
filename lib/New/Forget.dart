72d8cd9464faed64f626cc44e04bedc7e374b7d5
arora <aroramadhav65@gmail.com>
2022-03-22 23:05:42 +0530

    Update

--- lib/New/Forget.dart
+++ lib/New/Forget.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors, sized_box_for_whitespace
+// ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors, sized_box_for_whitespace, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:whatsapp/Constants/Constants.dart';
