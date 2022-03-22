758f2e4a5f8bb18b6f6feafb4993e0f3454aece0
arora <aroramadhav65@gmail.com>
2022-03-22 23:02:09 +0530

    Update

--- lib/New/Forget.dart
+++ lib/New/Forget.dart
@@ -26,7 +26,6 @@ class _LoginScreenState extends State<ForgetScreen> {
     );
 
     return Scaffold(
-      // backgroundColor: Colors.transparent,
       body: SingleChildScrollView(
         child: Container(
           width: MediaQuery.of(context).size.width,
