d10447e36c23f44b7ff22784e1b6359b319e94d6
arora <aroramadhav65@gmail.com>
2022-03-22 20:17:11 +0530

    Update

--- lib/New/Register.dart
+++ lib/New/Register.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase_storage/firebase_storage.dart';
