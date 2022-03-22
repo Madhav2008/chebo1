918129c18040744c694883334f0e5d73702c754c
arora <aroramadhav65@gmail.com>
2022-03-22 20:18:20 +0530

    Update

--- lib/New/Register.dart
+++ lib/New/Register.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors_in_immutables, file_names
 
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
