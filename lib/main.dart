4c3d7592990247f67f7a87d4ab78a2408564669b
arora <aroramadhav65@gmail.com>
2022-03-22 19:11:30 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,7 @@ class MyApp extends StatelessWidget {
             themeMode: themeProvider.themeMode,
             theme: MyThemes.lightTheme,
             darkTheme: MyThemes.darkTheme,
-            home: LoginScreen(),
+            home: SplashScreen(),
             // ),
           );
         },
