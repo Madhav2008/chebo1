7196834767a98577338dfafbba3b140443135642
arora <aroramadhav65@gmail.com>
2022-03-23 22:20:01 +0530

    Update

--- lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
+++ lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
@@ -26,7 +26,18 @@ class _SelectAccountScreenState extends State<SelectAccountScreen> {
           return InkWell(
             onTap: () {
               sourceChat = dummyData.removeAt(index);
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (builder)=>NavigationScreen(cameras: [], name: 'name', avatar: 'avatar', phoneno: 'phoneno', countryCode: 'countryCode', about: 'about',),))
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (builder) => NavigationScreen(
+                      cameras: [],
+                      name: 'name',
+                      avatar: 'avatar',
+                      phoneno: 'phoneno',
+                      countryCode: 'countryCode',
+                      about: 'about',
+                    ),
+                  ));
             },
             child: ListTile(
               leading: CircleAvatar(
