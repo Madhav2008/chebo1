4e8d2d86d2fdd1f9400463f5f4c69d078bc8dce8
arora <aroramadhav65@gmail.com>
2022-03-23 20:42:36 +0530

    Update

--- lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
+++ lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
@@ -1,3 +1,19 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+
+class SelectAccountScreen extends StatefulWidget {
+  const SelectAccountScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<SelectAccountScreen> createState() => _SelectAccountScreenState();
+}
+
+class _SelectAccountScreenState extends State<SelectAccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
