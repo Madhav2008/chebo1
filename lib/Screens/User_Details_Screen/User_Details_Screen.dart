04bbb8e0aee938e62de5b2d03c1fa109ad7b2b72
arora <aroramadhav65@gmail.com>
2022-03-23 22:42:22 +0530

    Update

--- lib/Screens/User_Details_Screen/User_Details_Screen.dart
+++ lib/Screens/User_Details_Screen/User_Details_Screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:whatsapp/Constants/Constants.dart';
-import 'package:whatsapp/Models/Chat_Model.dart';
 import 'package:whatsapp/Screens/Navigation/Navigation.dart';
 
 class UserDetailsScreen extends StatelessWidget {
