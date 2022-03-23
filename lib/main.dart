8a7bc5798746f26c9cd69b59619397861d7f569d
arora <aroramadhav65@gmail.com>
2022-03-23 21:58:55 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:whatsapp/Screens/Select_Account_Screen/Select_Account_Screen.dart';
 // import 'package:whatsapp/Auth/Auth_Services.dart';
 // import 'package:whatsapp/New/SignIn.dart';
 // import 'package:whatsapp/Screens/Landing_Page/Landing_Page.dart';
@@ -137,7 +138,7 @@ class MyApp extends StatelessWidget {
             themeMode: themeProvider.themeMode,
             theme: MyThemes.lightTheme,
             darkTheme: MyThemes.darkTheme,
-            home: SplashScreen(),
+            home: SelectAccountScreen(),
             // ),
           );
         },
