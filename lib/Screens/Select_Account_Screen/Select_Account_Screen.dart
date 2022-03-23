3c94166bf7034a8553f0e5fcc27090de433c2ad7
arora <aroramadhav65@gmail.com>
2022-03-23 20:45:20 +0530

    Update

--- lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
+++ lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp/Models/Chat_Model.dart';
 
 class SelectAccountScreen extends StatefulWidget {
   const SelectAccountScreen({ Key? key }) : super(key: key);
@@ -13,7 +14,7 @@ class _SelectAccountScreenState extends State<SelectAccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: ,),
+      body: ListView.builder(itemCount: dummyData.length ,itemBuilder: ,),
     );
   }
 }
