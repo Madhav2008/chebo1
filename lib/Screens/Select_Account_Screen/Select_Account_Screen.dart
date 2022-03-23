b00a647153c67c8ab0e7c14e9e3112b181386cd8
arora <aroramadhav65@gmail.com>
2022-03-23 21:53:08 +0530

    Update

--- lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
+++ lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp/Models/Chat_Model.dart';
 
 class SelectAccountScreen extends StatefulWidget {
-  const SelectAccountScreen({Key? key}) : super(key: key);
+  SelectAccountScreen({Key? key}) : super(key: key);
 
   @override
   State<SelectAccountScreen> createState() => _SelectAccountScreenState();
