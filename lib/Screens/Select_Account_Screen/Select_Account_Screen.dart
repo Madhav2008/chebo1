7719b0ec7c9a6161be6d2cfef3b2c2189a71f326
arora <aroramadhav65@gmail.com>
2022-03-24 20:07:48 +0530

    Update

--- lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
+++ lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:whatsapp/Models/Chat_Model.dart';
+// import 'package:whatsapp/Models/Chat_Model.dart';
 
 class SelectAccountScreen extends StatefulWidget {
   SelectAccountScreen({
