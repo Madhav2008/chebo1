69b9a3b5b68f198cef3649c749a338db6fede270
arora <aroramadhav65@gmail.com>
2022-03-24 19:53:46 +0530

    Update

--- lib/Widgets/Circular_Progress.dart
+++ lib/Widgets/Circular_Progress.dart
@@ -1,3 +1,19 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+
+class LoadingWhatsApp extends StatefulWidget {
+  const LoadingWhatsApp({ Key? key }) : super(key: key);
+
+  @override
+  State<LoadingWhatsApp> createState() => _LoadingWhatsAppState();
+}
+
+class _LoadingWhatsAppState extends State<LoadingWhatsApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
