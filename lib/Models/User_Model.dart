d0e30ee9d66aa78a534ccd639718b6090ce8593a
arora <aroramadhav65@gmail.com>
2022-03-26 20:49:43 +0530

    Update

--- lib/Models/User_Model.dart
+++ lib/Models/User_Model.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: file_names
+
 class User {
   final int id;
   final String name;
+  final String imageUrl;
 
   User({
     required this.id,
     required this.name,
+    required this.imageUrl,
   });
 }
