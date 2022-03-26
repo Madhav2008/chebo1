2ebe31abf69c3daf35deaa58a7ff17694b5d6900
arora <aroramadhav65@gmail.com>
2022-03-26 20:31:02 +0530

    Update

--- lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
+++ lib/Screens/Select_Account_Screen/Select_Account_Screen.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors
 
-import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:whatsapp/Widgets/Circular_Progress.dart';
-// import 'package:whatsapp/Models/Chat_Model.dart';
+import 'package:whatsapp/Models/Chat_Model.dart';
 
 class SelectAccountScreen extends StatefulWidget {
   SelectAccountScreen({
@@ -31,91 +28,46 @@ class _SelectAccountScreenState extends State<SelectAccountScreen> {
   Widget build(BuildContext context) {
     // ChatModel sourceChat;
     return Scaffold(
-      body: GestureDetector(
-        onTap: controlSignIn,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 270,
-              height: 65,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    '../assets/images/google_signin_button.png',
+      body: ListView.builder(
+        itemCount: dummyData.length,
+        itemBuilder: (
+          context,
+          index,
+        ) {
+          return InkWell(
+            onTap: () {
+              sourceChat = dummyData.removeAt(index);
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (builder) => NavigationScreen(
+                    cameras: [],
+                    name: dummyData[index].name,
+                    avatar: dummyData[index].avatarUrl,
+                    phoneno: dummyData[index].phoneno,
+                    countryCode: '+91',
+                    about: dummyData[index].about,
+                    sourceChat: sourceChat,
+                    chatModels: dummyData,
                   ),
-                  fit: BoxFit.cover,
                 ),
+              );
+            },
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Icon(
+                  Icons.person,
+                  color: white,
+                  size: 30,
+                ),
+                backgroundColor: one,
               ),
+              title: Text(dummyData[index].name),
             ),
-            Padding(
-              padding: EdgeInsets.all(
-                10.0,
-              ),
-              child: isLoading ? loadingWhatsAppIndia() : Container(),
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      // ListView.builder(
-      //   itemCount: dummyData.length,
-      //   itemBuilder: (
-      //     context,
-      //     index,
-      //   ) {
-      //     return InkWell(
-      //       onTap: () {
-      //         sourceChat = dummyData.removeAt(index);
-      //         Navigator.pushReplacement(
-      //           context,
-      //           MaterialPageRoute(
-      //             builder: (builder) => NavigationScreen(
-      //               cameras: [],
-      //               name: dummyData[index].name,
-      //               avatar: dummyData[index].avatarUrl,
-      //               phoneno: dummyData[index].phoneno,
-      //               countryCode: '+91',
-      //               about: dummyData[index].about,
-      //               sourceChat: sourceChat,
-      //               chatModels: dummyData,
-      //             ),
-      //           ),
-      //         );
-      //       },
-      //       child: ListTile(
-      //         leading: CircleAvatar(
-      //           child: Icon(
-      //             Icons.person,
-      //             color: white,
-      //             size: 30,
-      //           ),
-      //           backgroundColor: one,
-      //         ),
-      //         title: Text(dummyData[index].name),
-      //       ),
-      //     );
-      //   },
-      // ),
     );
-  }
-
-  Future<Null> controlSignIn() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuthentication =
-        await googleUser!.authentication;
-
-    // final AuthCredential credential = GoogleAuthProvider.getCredential(
-    //   idToken: googleAuthentication.idToken,
-    //   accessToken: googleAuthentication.accessToken,
-    // );
-
-    // FirebaseUser firebaseUser =
-    //     (await FirebaseAuth.signInWithCredential(credential)).user;
   }
 }
 
