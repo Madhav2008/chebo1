d1aaadb1c7e2b9564a9696468e694844603e9321
arora <aroramadhav65@gmail.com>
2022-03-26 23:15:38 +0530

    Update

--- lib/Models/User_Model.dart
+++ lib/Models/User_Model.dart
@@ -4,10 +4,12 @@ class User {
   final int id;
   final String name;
   final String imageUrl;
+  final String message;
 
   User({
     required this.id,
     required this.name,
     required this.imageUrl,
+    required this.message,
   });
 }
