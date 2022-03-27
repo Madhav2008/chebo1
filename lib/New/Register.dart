71eaa74b6349aea327a2287f4f892c7dae40c5d6
arora <aroramadhav65@gmail.com>
2022-03-27 14:19:18 +0530

    Update

--- lib/New/Register.dart
+++ lib/New/Register.dart
@@ -15,8 +15,6 @@ import 'package:whatsapp/Constants/Constants.dart';
 import 'package:whatsapp/Screens/Login_Screen/Login_Screen.dart';
 import 'package:whatsapp/Screens/Navigation/Navigation.dart';
 import 'package:whatsapp/Screens/Payment_Screen/Payment_Screen.dart';
-import 'package:whatsapp/main.dart';
-
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
