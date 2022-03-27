b107bdfd5041a2f4804287d83c99be1a50f1c4cc
arora <aroramadhav65@gmail.com>
2022-03-27 14:12:12 +0530

    Update

--- lib/New/Register.dart
+++ lib/New/Register.dart
@@ -389,7 +389,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               countryCode: '',
                               name: nameController.text,
                               phoneno: '',
-                              sourceChat: null,
+                              sourceChat:null,
                             ),
                           ),
                         );
