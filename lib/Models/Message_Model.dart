9bf8aeebdf424e7945ba03c01e34d12bbf0cbbdd
arora <aroramadhav65@gmail.com>
2022-03-27 13:21:36 +0530

    Update

--- lib/Models/Message_Model.dart
+++ lib/Models/Message_Model.dart
@@ -4,14 +4,12 @@ import 'package:whatsapp/Models/User_Model.dart';
 
 class MessageModel {
   String type;
-  User sender;
   String message;
   String time;
   bool unread;
   MessageModel({
     required this.message,
     required this.type,
-    required this.sender,
     required this.time,
     required this.unread,
   });
