f3a3e30be574020c2e5829bf80da37e3c883d348
arora <aroramadhav65@gmail.com>
2022-03-27 14:09:49 +0530

    Update

--- lib/New/Register.dart
+++ lib/New/Register.dart
@@ -43,7 +43,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       // bio: bioController.text,
       // userEmail: emailController.text,
       // contact: contactController.text,
-      img: uploadedPath,
+      // img: uploadedPath,
     );
   }
 
