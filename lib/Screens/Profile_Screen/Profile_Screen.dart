7bc181f2933005add27cccc7d7438cd18db2f1ef
arora <aroramadhav65@gmail.com>
2022-03-27 23:05:34 +0530

    Update

--- lib/Screens/Profile_Screen/Profile_Screen.dart
+++ lib/Screens/Profile_Screen/Profile_Screen.dart
@@ -153,7 +153,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         backgroundColor: grey,
                         backgroundImage: FileImage(
                           widget.avatar.isNotEmpty
-                          ?
+                          ? widget.avatar
                           : 'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
                         ),
                       ),
