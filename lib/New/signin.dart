c19c13b5bb23d66e46593c1b8c5490fcaf29047e
arora <aroramadhav65@gmail.com>
2022-03-28 19:20:18 +0530

    Update

--- lib/New/signin.dart
+++ lib/New/signin.dart
@@ -23,10 +23,11 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  static Future<User?> loginUsingEmailAndPassword(
-      {required String email,
-      required String password,
-      required BuildContext context,}) async {
+  static Future<User?> loginUsingEmailAndPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
     FirebaseAuth auth = FirebaseAuth.instance;
   }
 
