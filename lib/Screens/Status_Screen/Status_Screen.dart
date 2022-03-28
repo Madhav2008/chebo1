219e99de0da5c63e2716ad7367c01f12783223ac
arora <aroramadhav65@gmail.com>
2022-03-28 14:15:09 +0530

    Update

--- lib/Screens/Status_Screen/Status_Screen.dart
+++ lib/Screens/Status_Screen/Status_Screen.dart
@@ -55,7 +55,7 @@ class StatusScreen extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (builder) => CameraScreen(
-                        // cameras: [],
+                        cameras: [],
                         ),
                   ),
                 );
