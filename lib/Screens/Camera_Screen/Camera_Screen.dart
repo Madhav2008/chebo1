a6a4d8db14c88499eb690b8f0dcb03e6673264a0
arora <aroramadhav65@gmail.com>
2022-03-28 14:39:47 +0530

    Update

--- lib/Screens/Camera_Screen/Camera_Screen.dart
+++ lib/Screens/Camera_Screen/Camera_Screen.dart
@@ -55,7 +55,9 @@ class _CameraScreenState extends State<CameraScreen> {
             width: 70.0,
             height: barHeight - vertPadding * 2,
             child: GestureDetector(
-              onTap: (){},
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (builder)=>))
+              },
               child: Image(
                 image: NetworkImage(
                     'https://raw.githubusercontent.com/ng-kode/whatsapp_clone/master/assets/150x150.png'),
