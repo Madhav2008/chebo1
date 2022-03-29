25be9cc0ca39d6c6d73ac22827f81872c5e86d82
arora <aroramadhav65@gmail.com>
2022-03-29 20:44:24 +0530

    Update

--- lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
+++ lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
@@ -47,7 +47,7 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
       phoneNumber: phoneNumber,
       verificationCompleted: (phonesAuthCredentials) async {},
       verificationFailed: (verificationFailed) async {},
-      codeSent: (verificationId, resendingToken) async{},
+      codeSent: (verificationId, resendingToken) async {},
       codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
     );
   }
