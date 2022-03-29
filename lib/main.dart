27e8c642c86f78b09daec6fb944ea050141e5341
arora <aroramadhav65@gmail.com>
2022-03-29 22:18:52 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ late List<CameraDescription> cameras;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(
     MyApp(),
   );
