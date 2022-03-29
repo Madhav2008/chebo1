5d4e920c79d03dc3c4aede947f4a7e170b409d9f
arora <aroramadhav65@gmail.com>
2022-03-29 20:21:20 +0530

    Update

--- lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
+++ lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
@@ -192,7 +192,7 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
                 ),
                 Row(
                   children: [],
-                )
+                ),
               ],
             )
           : Column(
