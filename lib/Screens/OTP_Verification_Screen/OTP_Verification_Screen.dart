879e74313546a14c6ea6da18b09d079affdf4011
arora <aroramadhav65@gmail.com>
2022-03-29 20:50:03 +0530

    Update

--- lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
+++ lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
@@ -65,7 +65,7 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
       await _auth
           .signInWithCredential(credential)
           .then((value) {})
-          .whenComplete(() => null);
+          .whenComplete(() => null,);
     }
   }
 
