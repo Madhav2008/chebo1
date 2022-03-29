48a3777140b767125fa66410ae93590285c737bb
arora <aroramadhav65@gmail.com>
2022-03-29 20:54:05 +0530

    Update

--- lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
+++ lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
@@ -65,12 +65,15 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
       await _auth
           .signInWithCredential(credential)
           .then((value) {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (builder) {
-                return UserDetailsScreen(
-                    no: widget.no, countryCode: widget.countryCode);
-              },
-            ),);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (builder) {
+                  return UserDetailsScreen(
+                      no: widget.no, countryCode: widget.countryCode);
+                },
+              ),
+            );
           })
           .whenComplete(
             () => null,
