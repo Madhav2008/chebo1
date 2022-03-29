c870847ee6f224ac60eb28cada25db9a500bbd6f
arora <aroramadhav65@gmail.com>
2022-03-29 20:07:18 +0530

    Update

--- lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
+++ lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
@@ -170,7 +170,7 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
                   ),
                 ),
                 Text("The code used is invalid!"),
-                ElevatedButton(onPressed: (){}, child: child)
+                ElevatedButton(onPressed: (){}, child: Text('Edit Number'))
               ],
             ),
       // Center(
