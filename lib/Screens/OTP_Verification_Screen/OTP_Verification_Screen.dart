fc07a75fae8f86ab9679851a14503bc41d419ae4
arora <aroramadhav65@gmail.com>
2022-03-29 20:59:19 +0530

    Update

--- lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
+++ lib/Screens/OTP_Verification_Screen/OTP_Verification_Screen.dart
@@ -248,7 +248,9 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
                   children: [
                     Text("Didn't receive a OTP?"),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        _verifyPhoneNumber();
+                      },
                       child: Text('Resend OTP'),
                     ),
                   ],
