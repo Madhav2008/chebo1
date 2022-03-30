3b61c4c5040f3afabb5b9e2a4a98e769eccff3d5
arora <aroramadhav65@gmail.com>
2022-03-30 19:57:56 +0530

    Update

--- lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
+++ lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
@@ -1,21 +1,104 @@
-// ignore_for_file: file_names
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, file_names, prefer_const_constructors_in_immutables
 
-import 'package:firebase/firebase.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:whatsapp/Constants/Constants.dart';
 
-class ViewChatPhoto extends StatefulWidget {
-  const ViewChatPhoto({ Key? key }) : super(key: key);
+class CameraViewPage extends StatelessWidget {
+  CameraViewPage({Key? key, required this.path}) : super(key: key);
+  final String path;
 
-  @override
-  State<ViewChatPhoto> createState() => _ViewChatPhotoState();
-}
-
-class _ViewChatPhotoState extends State<ViewChatPhoto> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: black,
       appBar: AppBar(
-        
+        backgroundColor: black,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.crop_rotate,
+              size: 27,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.emoji_emotions_outlined,
+              size: 27,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.title,
+              size: 27,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.edit,
+              size: 27,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height - 150,
+              child: Image.file(
+                File(path),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                color: black,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                child: TextFormField(
+                  style: TextStyle(
+                    color: white,
+                    fontSize: 17,
+                  ),
+                  maxLines: 6,
+                  minLines: 1,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Add Caption....",
+                    prefixIcon: Icon(
+                      Icons.add_photo_alternate,
+                      color: white,
+                      size: 27,
+                    ),
+                    hintStyle: TextStyle(
+                      color: white,
+                      fontSize: 17,
+                    ),
+                    suffixIcon: CircleAvatar(
+                      radius: 27,
+                      backgroundColor: one,
+                      child: Icon(
+                        Icons.check,
+                        color: white,
+                        size: 27,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
