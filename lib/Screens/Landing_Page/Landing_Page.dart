67c9c1872bea50e4db012f040dbefb736925fced
arora <aroramadhav65@gmail.com>
2022-03-30 15:36:37 +0530

    Update

--- lib/Screens/Landing_Page/Landing_Page.dart
+++ lib/Screens/Landing_Page/Landing_Page.dart
@@ -101,7 +101,7 @@ class LandingPage extends StatelessWidget {
                       'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
                   phoneno: '9999348666',
                   countryCode: '+91',
-                  about: '👋🏻 Hey! there I am using WhatsApp India.',
+                  about: '👋🏻 Hey! there I am using WhatsApp India.', chatModels: [],
                 );
                     },
                   ),
