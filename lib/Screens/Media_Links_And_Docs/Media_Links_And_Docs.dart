9f762febee749e513c03ee2f8d5fd06684748057
arora <aroramadhav65@gmail.com>
2022-03-30 20:47:31 +0530

    Update

--- lib/Screens/Media_Links_And_Docs/Media_Links_And_Docs.dart
+++ lib/Screens/Media_Links_And_Docs/Media_Links_And_Docs.dart
@@ -82,7 +82,7 @@ class _MediaLinksAndDocsState extends State<MediaLinksAndDocs>
                 ),
                 
               );
-            }).toList()),,
+            }).toList()),
               ),
               Center(
                 child: Text('Docs'),
