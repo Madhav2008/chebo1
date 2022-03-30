4f3069bccdf12b3c4b2a2f06990fcde088fb9e47
arora <aroramadhav65@gmail.com>
2022-03-30 20:26:52 +0530

    Update

--- lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
+++ lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
@@ -31,7 +31,7 @@ class _ViewChatPhotoState extends State<ViewChatPhoto> {
       appBar: AppBar(
         backgroundColor: black,
         title: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               widget.senderName,
