b6b3a24074e47ca1e8685615d1f6e213f2efca1c
arora <aroramadhav65@gmail.com>
2022-03-30 20:15:35 +0530

    Update

--- lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
+++ lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
@@ -66,7 +66,7 @@ class _ViewChatPhotoState extends State<ViewChatPhoto> {
                     color: white,
                   );
                 });
-              }else{
+              } else {
                 setState(() {
                   star = Icon(
                     Icons.star_border,
