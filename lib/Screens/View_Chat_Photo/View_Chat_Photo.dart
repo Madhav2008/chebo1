6eddd9f28084a7bee1e69f685f61ed11a7541ea7
arora <aroramadhav65@gmail.com>
2022-03-30 20:07:46 +0530

    Update

--- lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
+++ lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
@@ -23,7 +23,7 @@ class ViewChatPhoto extends StatelessWidget {
       backgroundColor: black,
       appBar: AppBar(
         backgroundColor: black,
-        title: Text(''),
+        title: Text(senderName),
         actions: [
           IconButton(
             icon: Icon(
