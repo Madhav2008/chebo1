cfa569499290d1a7d3999f790d389abf077ac1b6
arora <aroramadhav65@gmail.com>
2022-03-30 16:35:34 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -448,11 +448,28 @@ class _ChatScreenState extends State<ChatScreen> {
                             ],
                           );
                         } else {
-                          return ReplyCard(
-                            color: reply,
-                            textColor: text,
-                            message: allMessages[index].message,
-                            messageTime: allMessages[index].time,
+                          return GestureDetector(
+                                onTap: () {
+                                  if (messageColor == blue.withOpacity(0.5)) {
+                                    setState(() {
+                                      messageColor = transparent;
+                                    });
+                                  }
+                                },
+                                onLongPress: () {
+                                  setState(() {
+                                    messageColor = blue.withOpacity(0.5);
+                                  });
+                                },
+                                child: Container(
+                                  color: messageColor,
+                                  width: MediaQuery.of(context).size.width,
+                                  child: ReplyCard(
+                              color: reply,
+                              textColor: text,
+                              message: allMessages[index].message,
+                              messageTime: allMessages[index].time,
+                            ),),
                           );
                         }
                       },
