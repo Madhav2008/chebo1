a79b6d16f4b5ab9d354db7c44967a65cdcc161b5
arora <aroramadhav65@gmail.com>
2022-03-30 15:47:15 +0530

    Update

--- lib/Screens/Linked_Devices/Linked_Devices.dart
+++ lib/Screens/Linked_Devices/Linked_Devices.dart
@@ -47,8 +47,8 @@ class _LinkedDevicesState extends State<LinkedDevices> {
                   phoneno: widget.phoneno,
                   avatar: widget.avatar,
                   countryCode: widget.countryCode,
-                  sourceChat: widget.sourceChat,
-                  chatModels: widget.chatModels,
+                  // sourceChat: widget.sourceChat,
+                  // chatModels: widget.chatModels,
                 ),
               ),
             );
