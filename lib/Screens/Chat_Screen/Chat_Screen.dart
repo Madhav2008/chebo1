1b915c0fb99330ccadcebadf4bb4959743df7a24
arora <aroramadhav65@gmail.com>
2022-03-30 16:34:04 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -410,11 +410,15 @@ class _ChatScreenState extends State<ChatScreen> {
                                     messageColor = blue.withOpacity(0.5);
                                   });
                                 },
-                                child: OwnMessageCard(
-                                  color: own,
-                                  textColor: text,
-                                  message: allMessages[index].message,
-                                  messageTime: allMessages[index].time,
+                                child: Container(
+                                  color: messageColor,
+                                  width: MediaQuery.of(context).size.width,
+                                  child: OwnMessageCard(
+                                    color: own,
+                                    textColor: text,
+                                    message: allMessages[index].message,
+                                    messageTime: allMessages[index].time,
+                                  ),
                                 ),
                               ),
                               ReplyCard(
