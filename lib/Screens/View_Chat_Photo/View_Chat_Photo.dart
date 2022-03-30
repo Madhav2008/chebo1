1769a91ab6ad251b0575d1e70e7bb649c0b76a36
arora <aroramadhav65@gmail.com>
2022-03-30 19:55:36 +0530

    Update

--- lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
+++ lib/Screens/View_Chat_Photo/View_Chat_Photo.dart
@@ -1,3 +1,19 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+
+class ViewChatPhoto extends StatefulWidget {
+  const ViewChatPhoto({ Key? key }) : super(key: key);
+
+  @override
+  State<ViewChatPhoto> createState() => _ViewChatPhotoState();
+}
+
+class _ViewChatPhotoState extends State<ViewChatPhoto> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
