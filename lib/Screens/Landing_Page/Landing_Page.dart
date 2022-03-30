f1c1d55c8465a83ea276b6414f8cbd62cd44f6ea
arora <aroramadhav65@gmail.com>
2022-03-30 15:36:12 +0530

    Update

--- lib/Screens/Landing_Page/Landing_Page.dart
+++ lib/Screens/Landing_Page/Landing_Page.dart
@@ -95,14 +95,14 @@ class LandingPage extends StatelessWidget {
                   MaterialPageRoute(
                     builder: (BuildContext context) {
                       return NavigationScreen(
-          //         cameras: cameras,
-          //         name: 'Madhav Arora',
-          //         avatar:
-          //             'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
-          //         phoneno: '9999348666',
-          //         countryCode: '+91',
-          //         about: '👋🏻 Hey! there I am using WhatsApp India.',
-          //       ),();
+                  cameras: cameras,
+                  name: 'Madhav Arora',
+                  avatar:
+                      'https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4',
+                  phoneno: '9999348666',
+                  countryCode: '+91',
+                  about: '👋🏻 Hey! there I am using WhatsApp India.',
+                );
                     },
                   ),
                 );
