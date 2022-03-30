5889f1908fb0187615d05979464458e22df3210f
arora <aroramadhav65@gmail.com>
2022-03-30 20:31:54 +0530

    Update

--- lib/Screens/View_Profile/View_Profile.dart
+++ lib/Screens/View_Profile/View_Profile.dart
@@ -57,7 +57,7 @@ class ViewProfile extends StatelessWidget {
                       path:
                           'https://raw.githubusercontent.com/ng-kode/whatsapp_clone/master/assets/150x150.png',
                       senderName: name,
-                      time: DateTime.now().toString(),
+                      time: DateTime.now().toString().substring(0,17),
                     ),
                   ),
                 );
