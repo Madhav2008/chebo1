97b124feb464b9082136dc269455c03bee58f63a
arora <aroramadhav65@gmail.com>
2022-03-31 17:02:58 +0530

    Update

--- lib/Screens/Login_Screen/Login_Screen.dart
+++ lib/Screens/Login_Screen/Login_Screen.dart
@@ -307,7 +307,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   // exclude: <String>['KN', 'MF'],
                   //Optional. Shows phone code before the country name.
                   showPhoneCode: true,
-                  showWorldWide: false,
                   onSelect: (Country country) {
                     print(
                       'Select country: ${country.displayName}',
