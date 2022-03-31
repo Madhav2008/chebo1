0e143605577b54759dfa9d3c91f68fe8282fb7c2
arora <aroramadhav65@gmail.com>
2022-03-31 17:17:41 +0530

    Update

--- lib/Screens/Login_Screen/Login_Screen.dart
+++ lib/Screens/Login_Screen/Login_Screen.dart
@@ -332,32 +332,6 @@ class _LoginScreenState extends State<LoginScreen> {
                 //     ),
                 //   ),
                 // );
-                showCountryPicker(
-                  context: context,
-                  countryListTheme: CountryListThemeData(
-                    flagSize: 25,
-                    // backgroundColor: Colors.white,
-                    textStyle: TextStyle(fontSize: 16, color: Colors.blueGrey),
-                    //Optional. Sets the border radius for the bottomsheet.
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.0),
-                      topRight: Radius.circular(20.0),
-                    ),
-                    //Optional. Styles the search field.
-                    inputDecoration: InputDecoration(
-                      labelText: 'Search',
-                      hintText: 'Start typing to search',
-                      prefixIcon: const Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: const Color(0xFF8C98A8).withOpacity(0.2),
-                        ),
-                      ),
-                    ),
-                  ),
-                  onSelect: (Country country) =>
-                      print('Select country: ${country.displayName}'),
-                );
               },
               child: Container(
                 width: MediaQuery.of(context).size.width - 100,
