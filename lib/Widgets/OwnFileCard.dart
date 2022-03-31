0b8883ddcd69952df99102ce46d868e1183a4750
arora <aroramadhav65@gmail.com>
2022-03-31 11:03:35 +0530

    Update

--- lib/Widgets/OwnFileCard.dart
+++ lib/Widgets/OwnFileCard.dart
@@ -1,3 +1,14 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+
+class OwnFileCard extends StatelessWidget {
+  const OwnFileCard({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
