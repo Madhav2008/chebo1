b84853ea7867f238b53c8e6ab9468383e037fb42
arora <aroramadhav65@gmail.com>
2022-03-31 11:24:20 +0530

    Update

---
+++ lib/Widgets/ReplyFileCard.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_constructors_in_immutables
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:whatsapp/Constants/Constants.dart';
+
+class ReplyFileCard extends StatelessWidget {
+  ReplyFileCard({
+    Key? key,
+    required this.path,
+  }) : super(key: key);
+
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2.3,
+          width: MediaQuery.of(context).size.width / 1.8,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: one,
+          ),
+          child: Card(
+            margin: EdgeInsets.all(
+              3,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Image.file(
+              File(path),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
