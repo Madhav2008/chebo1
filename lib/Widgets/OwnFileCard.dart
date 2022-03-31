6e4278d6519e0d58ea4ff719e55034d5eb09b7c5
arora <aroramadhav65@gmail.com>
2022-03-31 11:04:35 +0530

    Update

--- lib/Widgets/OwnFileCard.dart
+++ lib/Widgets/OwnFileCard.dart
@@ -3,13 +3,17 @@
 import 'package:flutter/material.dart';
 
 class OwnFileCard extends StatelessWidget {
-  const OwnFileCard({ Key? key }) : super(key: key);
+  const OwnFileCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
-      child: Padding(padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5,)),
+      child: Padding(
+          padding: EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 5,
+      )),
     );
   }
 }
