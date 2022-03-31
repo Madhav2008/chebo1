aba345076b4aff8f629b2d211e79f97d5dcc91a8
arora <aroramadhav65@gmail.com>
2022-03-31 11:03:55 +0530

    Update

--- lib/Widgets/OwnFileCard.dart
+++ lib/Widgets/OwnFileCard.dart
@@ -7,8 +7,8 @@ class OwnFileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Align(
+      alignment: Alignment.centerRight,
     );
   }
 }
