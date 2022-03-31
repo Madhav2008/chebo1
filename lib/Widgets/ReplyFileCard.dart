edd725ffb3c2e72b90489b61949080775ba3bbd1
arora <aroramadhav65@gmail.com>
2022-03-31 11:58:35 +0530

    Update

--- lib/Widgets/ReplyFileCard.dart
+++ lib/Widgets/ReplyFileCard.dart
@@ -29,7 +29,7 @@ class ReplyFileCard extends StatelessWidget {
           width: MediaQuery.of(context).size.width / 1.8,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
-            color: grey[400],
+            color: fileColor,
           ),
           child: Card(
             margin: EdgeInsets.all(
