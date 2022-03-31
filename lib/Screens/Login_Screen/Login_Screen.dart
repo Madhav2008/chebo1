7918f92c6e9fe845427dc67d0c165590d562ebc3
arora <aroramadhav65@gmail.com>
2022-03-31 16:49:53 +0530

    Update

--- lib/Screens/Login_Screen/Login_Screen.dart
+++ lib/Screens/Login_Screen/Login_Screen.dart
@@ -302,35 +302,35 @@ class _LoginScreenState extends State<LoginScreen> {
                 //   ),
                 // );
                 showCountryPicker(
-              context: context,
-              //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
-              exclude: <String>['KN', 'MF'],
-              //Optional. Shows phone code before the country name.
-              showPhoneCode: true,
-              showWorldWide: false,
-              onSelect: (Country country) {
-                print('Select country: ${country.displayName}');
-              },
-              // Optional. Sets the theme for the country list picker.
-              countryListTheme: CountryListThemeData(
-                // Optional. Sets the border radius for the bottomsheet.
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40.0),
-                ),
-                // Optional. Styles the search field.
-                inputDecoration: InputDecoration(
-                  labelText: 'Search',
-                  hintText: 'Start typing to search',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: const Color(0xFF8C98A8).withOpacity(0.2),
+                  context: context,
+                  //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
+                  exclude: <String>['KN', 'MF'],
+                  //Optional. Shows phone code before the country name.
+                  showPhoneCode: true,
+                  showWorldWide: false,
+                  onSelect: (Country country) {
+                    print('Select country: ${country.displayName}');
+                  },
+                  // Optional. Sets the theme for the country list picker.
+                  countryListTheme: CountryListThemeData(
+                    // Optional. Sets the border radius for the bottomsheet.
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40.0),
+                      topRight: Radius.circular(40.0),
+                    ),
+                    // Optional. Styles the search field.
+                    inputDecoration: InputDecoration(
+                      labelText: 'Search',
+                      hintText: 'Start typing to search',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: const Color(0xFF8C98A8).withOpacity(0.2),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            );
+                );
               },
               child: Container(
                 width: MediaQuery.of(context).size.width - 100,
