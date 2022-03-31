309aa66e3032a864fb47168ec2cbf1fab5f98713
arora <aroramadhav65@gmail.com>
2022-03-31 11:10:58 +0530

    Update

--- lib/Widgets/OwnFileCard.dart
+++ lib/Widgets/OwnFileCard.dart
@@ -34,7 +34,7 @@ class OwnFileCard extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15),
             ),
-            child: Image.file(File(path)),
+            child: Image.file(File(path),),
           ),
         ),
       ),
