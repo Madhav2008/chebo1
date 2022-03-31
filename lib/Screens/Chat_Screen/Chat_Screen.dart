f12f3c9355d020ea936809cfab28f91e5d0ebc42
arora <aroramadhav65@gmail.com>
2022-03-31 11:44:56 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -454,59 +454,60 @@ class _ChatScreenState extends State<ChatScreen> {
                                 )
                               ],
                             );
+                          } else {
+                            return Column(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    if (messageColor == blue.withOpacity(0.5)) {
+                                      setState(() {
+                                        messageColor = transparent;
+                                      });
+                                    }
+                                  },
+                                  onLongPress: () {
+                                    setState(() {
+                                      messageColor = blue.withOpacity(0.5);
+                                    });
+                                  },
+                                  child: Container(
+                                    color: messageColor,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: OwnMessageCard(
+                                      color: own,
+                                      textColor: text,
+                                      message: allMessages[index].message,
+                                      messageTime: allMessages[index].time,
+                                    ),
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    if (messageColor == blue.withOpacity(0.5)) {
+                                      setState(() {
+                                        messageColor = transparent;
+                                      });
+                                    }
+                                  },
+                                  onLongPress: () {
+                                    setState(() {
+                                      messageColor = blue.withOpacity(0.5);
+                                    });
+                                  },
+                                  child: Container(
+                                    color: messageColor,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: ReplyCard(
+                                      color: reply,
+                                      textColor: text,
+                                      message: allMessages[index].message,
+                                      messageTime: allMessages[index].time,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            );
                           }
-                          return Column(
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  if (messageColor == blue.withOpacity(0.5)) {
-                                    setState(() {
-                                      messageColor = transparent;
-                                    });
-                                  }
-                                },
-                                onLongPress: () {
-                                  setState(() {
-                                    messageColor = blue.withOpacity(0.5);
-                                  });
-                                },
-                                child: Container(
-                                  color: messageColor,
-                                  width: MediaQuery.of(context).size.width,
-                                  child: OwnMessageCard(
-                                    color: own,
-                                    textColor: text,
-                                    message: allMessages[index].message,
-                                    messageTime: allMessages[index].time,
-                                  ),
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  if (messageColor == blue.withOpacity(0.5)) {
-                                    setState(() {
-                                      messageColor = transparent;
-                                    });
-                                  }
-                                },
-                                onLongPress: () {
-                                  setState(() {
-                                    messageColor = blue.withOpacity(0.5);
-                                  });
-                                },
-                                child: Container(
-                                  color: messageColor,
-                                  width: MediaQuery.of(context).size.width,
-                                  child: ReplyCard(
-                                    color: reply,
-                                    textColor: text,
-                                    message: allMessages[index].message,
-                                    messageTime: allMessages[index].time,
-                                  ),
-                                ),
-                              )
-                            ],
-                          );
                         } else {
                           return GestureDetector(
                             onTap: () {
