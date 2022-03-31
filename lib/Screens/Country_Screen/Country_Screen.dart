590bebe10c9878f50d2a296305cd909bdce8ebc7
arora <aroramadhav65@gmail.com>
2022-03-31 16:47:31 +0530

    Update

--- lib/Screens/Country_Screen/Country_Screen.dart
+++ lib/Screens/Country_Screen/Country_Screen.dart
@@ -82,6 +82,36 @@ class _CountryScreenState extends State<CountryScreen> {
         ],
       ),
       body: 
+      showCountryPicker(
+              context: context,
+              //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
+              exclude: <String>['KN', 'MF'],
+              //Optional. Shows phone code before the country name.
+              showPhoneCode: true,
+              showWorldWide: false,
+              onSelect: (Country country) {
+                print('Select country: ${country.displayName}');
+              },
+              // Optional. Sets the theme for the country list picker.
+              countryListTheme: CountryListThemeData(
+                // Optional. Sets the border radius for the bottomsheet.
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                ),
+                // Optional. Styles the search field.
+                inputDecoration: InputDecoration(
+                  labelText: 'Search',
+                  hintText: 'Start typing to search',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color(0xFF8C98A8).withOpacity(0.2),
+                    ),
+                  ),
+                ),
+              ),
+            );
       // ListView.builder(
       //   itemCount: countries.length,
       //   itemBuilder: (
