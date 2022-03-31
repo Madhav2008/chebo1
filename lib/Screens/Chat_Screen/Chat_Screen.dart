d48c8b24c3535f41a7fe6ec04ae86d3e1edbbc7b
arora <aroramadhav65@gmail.com>
2022-03-31 11:41:59 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -405,6 +405,7 @@ class _ChatScreenState extends State<ChatScreen> {
                           );
                         }
                         if (allMessages[index].type == "source") {
+                          if(allMessages[index].path != null){}
                           return Column(
                             children: [
                               GestureDetector(
