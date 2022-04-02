5219ff50657d89066c8ee0b7759bcd9b19252d95
arora <aroramadhav65@gmail.com>
2022-04-02 20:05:43 +0530

    Update

--- lib/Screens/Profile_Screen/Profile_Screen.dart
+++ lib/Screens/Profile_Screen/Profile_Screen.dart
@@ -38,6 +38,7 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   String aboutss = '👋🏻 Hey! there I am using WhatsApp India.';
   final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _name1Controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     File? _imageFile;
