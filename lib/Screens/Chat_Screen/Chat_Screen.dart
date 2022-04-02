0b2a818e9b1e2cef4c38b1726de02f6d68d60d55
arora <aroramadhav65@gmail.com>
2022-04-02 20:47:07 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -505,7 +505,6 @@ class _ChatScreenState extends State<ChatScreen> {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
-                      //   child: Text("Start"),
                       //   color: Colors.green,
                       // ),
                       // FlatButton(
