ef8d3860d2ce1962489037585c9e64f2b4a28f35
arora <aroramadhav65@gmail.com>
2022-04-02 20:08:33 +0530

    Update

--- lib/Screens/Profile_Screen/Profile_Screen.dart
+++ lib/Screens/Profile_Screen/Profile_Screen.dart
@@ -354,7 +354,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                   ),
                                   GestureDetector(
                                     onTap: () {
-                                      _nameController.value = _name1Controller.value;
+                                      _nameController.value = _name1Controller.value'
                                       Navigator.pop(context);
                                     },
                                     child: Text(
