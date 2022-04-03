181de0a10e58e44b8d346e865f694cd6eb357176
arora <aroramadhav65@gmail.com>
2022-04-03 13:14:49 +0530

    Update

--- lib/Screens/WhatsApp_Video_Calling_Screen/WhatsApp_Video_Calling_Screen.dart
+++ lib/Screens/WhatsApp_Video_Calling_Screen/WhatsApp_Video_Calling_Screen.dart
@@ -184,7 +184,7 @@ class _WhatsAppVideoCallingScreenState
                           size: 28,
                         ),
                         onPressed: () {
-                          if(camera = false){}
+                          if (camera = false) {}
                           setState(() {
                             camera = !camera;
                           });
