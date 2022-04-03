916cc0236cefba0bdd8a03e8ffac77938e80b4cf
arora <aroramadhav65@gmail.com>
2022-04-03 11:56:36 +0530

    Update

--- lib/Screens/Change_Number_Screen_Two/Change_Number_Screen_Two.dart
+++ lib/Screens/Change_Number_Screen_Two/Change_Number_Screen_Two.dart
@@ -48,7 +48,7 @@ class _ChangeNumberScreenTwoState extends State<ChangeNumberScreenTwo> {
               child: Row(
                 children: [
                   GestureDetector(
-                    onTap: (){},
+                    onTap: () {},
                     child: Container(
                       decoration: BoxDecoration(
                         border: Border(
