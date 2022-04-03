4bb1fb7bc2a9a0e2a5d215074334bffaeecc48dc
arora <aroramadhav65@gmail.com>
2022-04-03 11:50:33 +0530

    Update

--- lib/Screens/Change_Number_Screen_Two/Change_Number_Screen_Two.dart
+++ lib/Screens/Change_Number_Screen_Two/Change_Number_Screen_Two.dart
@@ -35,7 +35,7 @@ class ChangeNumberScreenTwo extends StatelessWidget {
               height: 30,
             ),
             GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: Container(
                 width: MediaQuery.of(context).size.width - 100,
                 child: Row(
