f0d0ce4b868ead838c1d88108951b829f097b2c3
arora <aroramadhav65@gmail.com>
2022-04-03 11:58:06 +0530

    Update

--- lib/Screens/Delete_My_Account/Delete_My_Account.dart
+++ lib/Screens/Delete_My_Account/Delete_My_Account.dart
@@ -212,7 +212,7 @@ class _DeleteMyAccountState extends State<DeleteMyAccount> {
                           showPhoneCode: true,
                           onSelect: (Country country) {
                             setState(() {
-                              oldPhoneCountryCode = '+' + country.phoneCode;
+                              countryCode = '+' + country.phoneCode;
                             });
                             print(
                               'Display Name: ${country.displayName}, Country Code: ${country.countryCode}, Display Name No Country Code: ${country.displayNameNoCountryCode}, E163 Key: ${country.e164Key}, E164 SC: ${country.e164Sc}, Example: ${country.example}, Full example with plus sign: ${country.fullExampleWithPlusSign}, Level: ${country.level}, Name: ${country.name}, Name Localized: ${country.nameLocalized}, Phone Code: ${country.phoneCode}, Geographic: ${country.geographic}',
