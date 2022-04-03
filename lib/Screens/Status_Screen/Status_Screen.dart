06ddfb0d2679921105efb06107947ca327570f71
arora <aroramadhav65@gmail.com>
2022-04-03 15:58:43 +0530

    Update

--- lib/Screens/Status_Screen/Status_Screen.dart
+++ lib/Screens/Status_Screen/Status_Screen.dart
@@ -152,9 +152,9 @@ class StatusScreen extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) => StoryPageView(
-                      // name: name1,
-                      // avatar: avatar1,
-                    ),
+                        // name: name1,
+                        // avatar: avatar1,
+                        ),
                   ),
                 );
               },
@@ -175,9 +175,9 @@ class StatusScreen extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) => StoryPageView(
-                      // name: name2,
-                      // avatar: avatar2,
-                    ),
+                        // name: name2,
+                        // avatar: avatar2,
+                        ),
                   ),
                 );
               },
