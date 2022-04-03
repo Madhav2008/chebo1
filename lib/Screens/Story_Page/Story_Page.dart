709376a57b09143ad3307633f37a93b5e75587a4
arora <aroramadhav65@gmail.com>
2022-04-03 14:39:26 +0530

    Update

--- lib/Screens/Story_Page/Story_Page.dart
+++ lib/Screens/Story_Page/Story_Page.dart
@@ -55,7 +55,7 @@ class StoryPageView extends StatelessWidget {
 //   }
 // }
 
-  final StoryController controller = StoryController();
+  final _storyController controller = StoryController();
 
   @override
   Widget build(BuildContext context) {
