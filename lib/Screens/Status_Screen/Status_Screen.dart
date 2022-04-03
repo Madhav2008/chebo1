216eb2b35a8d3412d38f54722d933102dfae66e2
arora <aroramadhav65@gmail.com>
2022-04-03 14:43:41 +0530

    Update

--- lib/Screens/Status_Screen/Status_Screen.dart
+++ lib/Screens/Status_Screen/Status_Screen.dart
@@ -152,9 +152,9 @@ class StatusScreen extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) => StoryPageView(
-                      // name: name1,
-                      // avatar: avatar1,
-                    ),
+                        // name: name1,
+                        // avatar: avatar1,
+                        ),
                   ),
                 );
               },
