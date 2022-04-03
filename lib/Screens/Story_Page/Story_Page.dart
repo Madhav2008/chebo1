eaee0a87575ea64b70f9de0d57106598207e1499
arora <aroramadhav65@gmail.com>
2022-04-03 16:16:32 +0530

    Update

--- lib/Screens/Story_Page/Story_Page.dart
+++ lib/Screens/Story_Page/Story_Page.dart
@@ -39,37 +39,38 @@ class StoryPageView extends StatelessWidget {
       //   imageFit: BoxFit.contain,
       // ),
       StoryItem.text(
-            title: "I guess you'd love to see more of our food. That's great.",
-            backgroundColor: Colors.blue,
-          ),
-          StoryItem.text(
-            title: "Nice!\n\nTap to continue.",
-            backgroundColor: Colors.red,
-            textStyle: TextStyle(
-              fontFamily: 'Dancing',
-              fontSize: 40,
-            ),
-          ),
-          StoryItem.pageImage(
-            url:
-                "https://image.ibb.co/cU4WGx/Omotuo-Groundnut-Soup-braperucci-com-1.jpg",
-            caption: "Still sampling",
-            controller: storyController,
-          ),
-          StoryItem.pageImage(
-              url: "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
-              caption: "Working with gifs",
-              controller: storyController,),
-          StoryItem.pageImage(
-            url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
-            caption: "Hello, from the other side",
-            controller: storyController,
-          ),
-          StoryItem.pageImage(
-            url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
-            caption: "Hello, from the other side2",
-            controller: storyController,
-          ),
+        title: "I guess you'd love to see more of our food. That's great.",
+        backgroundColor: Colors.blue,
+      ),
+      StoryItem.text(
+        title: "Nice!\n\nTap to continue.",
+        backgroundColor: Colors.red,
+        textStyle: TextStyle(
+          fontFamily: 'Dancing',
+          fontSize: 40,
+        ),
+      ),
+      StoryItem.pageImage(
+        url:
+            "https://image.ibb.co/cU4WGx/Omotuo-Groundnut-Soup-braperucci-com-1.jpg",
+        caption: "Still sampling",
+        controller: storyController,
+      ),
+      StoryItem.pageImage(
+        url: "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
+        caption: "Working with gifs",
+        controller: storyController,
+      ),
+      StoryItem.pageImage(
+        url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
+        caption: "Hello, from the other side",
+        controller: storyController,
+      ),
+      StoryItem.pageImage(
+        url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
+        caption: "Hello, from the other side2",
+        controller: storyController,
+      ),
     ];
     return Scaffold(
       appBar: AppBar(
