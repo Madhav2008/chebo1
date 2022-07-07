39d6e0b32ad6fc63207a16ad888f1666a3054968
arora <aroramadhav65@gmail.com>
2022-07-07 19:04:10 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -140,7 +140,7 @@ class _ChatScreenState extends State<ChatScreen> {
             16,
           ),
       unread: read,
-      path: path,
+      // path: path,
     );
     setState(() {
       setState(() {
