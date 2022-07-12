b81a42d4408865e03310e237321a7d7ca2fa9749
arora <aroramadhav65@gmail.com>
2022-07-12 10:59:16 +0530

    Update

--- lib/Screens/Contact_Support/Contact_Support.dart
+++ lib/Screens/Contact_Support/Contact_Support.dart
@@ -116,7 +116,7 @@ class _ContactSupportState extends State<ContactSupport> {
                   ),
                 ),
                 GestureDetector(
-                  onTap: () {
+                  onTap: () {_bodyController.text.isEmpty
                     _launchURL();
                   },
                   child: Container(
