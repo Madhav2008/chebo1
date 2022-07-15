cbb9f79afb46b4c33f260a9eb6e95e59ca5c65b4
arora <aroramadhav65@gmail.com>
2022-07-15 19:07:51 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -400,28 +400,28 @@ class _ChatScreenState extends State<ChatScreen> {
                         }
                         if (allMessages[index].type == "source") {
                           if (allMessages[index].path != null) {
-                            // return GestureDetector(
-                            //   onTap: () {
-                            //     if (messageColor == blue.withOpacity(0.5)) {
-                            //       setState(() {
-                            //         messageColor = transparent;
-                            //       });
-                            //     }
-                            //   },
-                            //   onLongPress: () {
-                            //     setState(() {
-                            //       messageColor = blue.withOpacity(0.5);
-                            //     });
-                            //   },
-                            //   child: Container(
-                            //     color: messageColor,
-                            //     width: MediaQuery.of(context).size.width,
-                            //     child: OwnFileCard(
-                            //       path: allMessages[index].path,
-                            //       fileColor: own,
-                            //     ),
-                            //   ),
-                            // );
+                            return GestureDetector(
+                              onTap: () {
+                                if (messageColor == blue.withOpacity(0.5)) {
+                                  setState(() {
+                                    messageColor = transparent;
+                                  });
+                                }
+                              },
+                              onLongPress: () {
+                                setState(() {
+                                  messageColor = blue.withOpacity(0.5);
+                                });
+                              },
+                              child: Container(
+                                color: messageColor,
+                                width: MediaQuery.of(context).size.width,
+                                child: OwnFileCard(
+                                  path: allMessages[index].path,
+                                  fileColor: own,
+                                ),
+                              ),
+                            );
                           } else {
                             return GestureDetector(
                               onTap: () {
