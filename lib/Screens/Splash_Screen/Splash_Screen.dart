0ce6518d1371a55f358ade14202651d39b29db3f
arora <aroramadhav65@gmail.com>
2022-07-15 19:05:01 +0530

    Update

--- lib/Screens/Splash_Screen/Splash_Screen.dart
+++ lib/Screens/Splash_Screen/Splash_Screen.dart
@@ -46,8 +46,7 @@ class _SplashScreenState extends State<SplashScreen> {
     //     ? '../assets/images/WhatsAppIndiaLogo1.png'
     //     : '../assets/images/WhatsAppIndiaLogo.png';
         
-    final logo = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
-        ? '../assets/images/WhatsAppIndiaLogo1.png'
+    final logo = '../assets/images/WhatsAppIndiaLogo1.png'
         : '../assets/images/WhatsAppIndiaLogo.png';
 
     return FutureBuilder(
