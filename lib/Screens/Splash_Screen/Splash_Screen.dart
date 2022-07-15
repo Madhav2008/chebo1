137fcab1d666bf191e359c4e6d5f2831bb5f5bd8
arora <aroramadhav65@gmail.com>
2022-07-15 19:05:48 +0530

    Update

--- lib/Screens/Splash_Screen/Splash_Screen.dart
+++ lib/Screens/Splash_Screen/Splash_Screen.dart
@@ -41,6 +41,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+    
     final logo = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
         ? '../assets/images/WhatsAppIndiaLogo1.png'
         : '../assets/images/WhatsAppIndiaLogo.png';
