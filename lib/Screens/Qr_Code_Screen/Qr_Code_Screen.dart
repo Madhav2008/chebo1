552db5c83f9f15a7706e211fab9e8befb961849b
arora <aroramadhav65@gmail.com>
2022-07-28 23:11:49 +0530

    Update

--- lib/Screens/Qr_Code_Screen/Qr_Code_Screen.dart
+++ lib/Screens/Qr_Code_Screen/Qr_Code_Screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:share/share.dart';
