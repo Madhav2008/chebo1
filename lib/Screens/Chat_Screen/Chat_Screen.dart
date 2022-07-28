82304ab497272cbf27f543f60ac9d79b3e431df7
arora <aroramadhav65@gmail.com>
2022-07-28 22:48:35 +0530

    Update

--- lib/Screens/Chat_Screen/Chat_Screen.dart
+++ lib/Screens/Chat_Screen/Chat_Screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables, prefer_final_fields, sized_box_for_whitespace, file_names, use_key_in_widget_constructors, unnecessary_null_comparison
 
+import 'dart:html';
+
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -529,7 +531,7 @@ class _ChatScreenState extends State<ChatScreen> {
                                   maxLines: 2,
                                   minLines: 1,
                                   onChanged: (value) {
-                                    if (value.isNotEmpty || value != ' ') {
+                                    if (value.isNotEmpty || value != SpanElement.created()) {
                                       setState(() {
                                         sendButton = true;
                                       });
